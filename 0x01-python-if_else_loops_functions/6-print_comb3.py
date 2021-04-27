537dfdb046850ec3afa16061672676828d30a09d
BezK97 <bkifle97@gmail.com>
2021-04-27 18:12:12 +0000

    Number combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 for i in range(1, 90):
-    if i < 10:
-        print("0{:d}, ".format(i), end="")
     if i / 10 > i % 10:
         continue
-    if i >= 10 and i != 89:
-        print("{:d}, ".format(i), end="")
+    if i != 89:
+        print("{:02d}, ".format(i), end="")
     elif i == 89:
-        print("{:d}".format(i))
+        print("{:02d}".format(i))
