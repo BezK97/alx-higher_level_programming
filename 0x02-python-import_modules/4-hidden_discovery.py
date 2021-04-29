706b9b8a52925feb6ee82814483f42d5e898d7c8
BezK97 <bkifle97@gmail.com>
2021-04-29 12:27:23 +0000

    Print names defined in module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    name = dir(hidden_4)
+
+    for i in range(len(name)):
+        if name[i][:2] != "__":
+            print(name[i])
