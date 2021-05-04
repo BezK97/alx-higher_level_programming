c88ecd9de69048ec4ded508be620c7eee3e801c9
BezK97 <bkifle97@gmail.com>
2021-05-04 14:38:28 +0000

    Remove item from list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     element = my_list[idx]
+    if idx < 0 or idx >= len(my_list):
+        return my_list
     my_list.remove(element)
     return (my_list)
