fe354af7b59d34d9e67da170f13b38df1e100c36
BezK97 <bkifle97@gmail.com>
2021-05-04 12:12:07 +0000

    Copy list and modify

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0:
+        return (new_list)
+    if idx >= len(my_list):
+        return (new_list)
+    for i in range(len(my_list)):
+        if idx == i:
+            new_list[i] = element
+            return (new_list)
